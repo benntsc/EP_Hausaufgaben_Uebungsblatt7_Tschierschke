60d06a1d8487c85b84abe7b01d113cbcfacd77af
Benn Tschierschke <benn@8a968e48-a31a-4133-b1e5-08649a7d8484.fritz.box>
2025-12-05 12:43:14 +0100

    Hausaufgabe 2: Arrays verwenden

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,11 +1,25 @@
 package h2;
 
+import java.util.Arrays;
+
 public class H2_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-	
+
+	public static int[] change(int[] a, int[] b, int start, int end) {
+		if (end > start) {
+			if (Arrays.compare(a, b) != 0) {
+				return Arrays.copyOfRange(a, 0, a.length);
+			} else {
+				Arrays.sort(a);
+				return Arrays.copyOfRange(a, start, end);
+			}
+		} else {
+			return new int[0];
+		}
+	}
 
 }
